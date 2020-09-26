1ee0cbf7f81728a503068f92d086f1f270090673
Mateus Spadari <mateus_salvador2@hotmail.com>
2020-09-26 16:12:10 -0300

    Add alert message to user

--- Messages - Atividade 02/Mensagens/decoder.rb
+++ Messages - Atividade 02/Mensagens/decoder.rb
@@ -9,10 +9,13 @@ mensagens.each do |n|
   if n.include? "decoder"
     next
   end
+
   file = File.open(n)
   file_data = file.read
   plain = Base64.decode64(file_data)
   File.write("mensagem_#{n.scan(/\d/).join('')}_decoded.txt", plain)
   file.close
 end
+
+puts "Novos arquivos 'mensagem_xxx_decoded.txt' criados."
 
